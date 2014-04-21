d88ca8d57cd82cc9037bcb19a8ce824499773f8e
crewgn <crewgn@gmail.com>
2014-04-21 12:26:08 +0400

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.0.2'
+gem 'rails', '4.0.1'
+gem 'bootstrap-sass', '2.3.2.0'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
@@ -15,7 +16,7 @@ group :test do
 	gem 'capybara', '2.1.0'
 end
 
-gem 'sass-rails', '4.0.1'
+gem 'sass-rails', '~> 4.0.0'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '2.1.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,39 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    before { visit  root_path }
 
-    it "should have the title 'RoR'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_title('RoR')
-    end
-
-    it "should not have the title 'Home'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
-    
-end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title ('| Home') }
+      
+  end
 
 
-    describe "Help page" do
-    it "should have the content 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+  describe "Help page" do
+    before { visit  help_path }
 
-    it "should have the title 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      expect(page).to have_title('RoR | Help')
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+      
   end
 
   describe "About page" do
-    it "should have the content 'About us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      expect(page).to have_content('About us')
-    end
+    before { visit  about_path }
 
-    it "should have the title 'About'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      expect(page).to have_title('RoR | About')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+      
+  end
+
+  describe "Contact page" do
+    before { visit  contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+      
   end
 end
